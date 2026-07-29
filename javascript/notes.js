afa32fb297410e782fd6842ae824e05613c0374f
Hari G <gudhitiharitha2610@gmail.com>
2026-07-29 17:48:06 +0530

    javascript notes

---
+++ javascript/notes.js
@@ -0,0 +1,157 @@
+//introduction to javscript
+/*
+highlevel language,Interpreted,dynamically typed programing language
+primarily used to make web pages interactive
+without js:
+* weebsites are 
+why js?
+
+history:
+1995-js-brenden eich
+netscope
+
+mocha-live script
+Ecma=european computers manufactures association 
+java-javascript
+
+why it is called a scripting language
+difference b/w java and javascript
+
+mobile aps:vs code,
+descktop-
+server apps:
+tensor flow:
+iot
+ where we can run js
+ intialy - only crome,web bowser,firefox,edge
+ server:
+ mobile
+ iot-
+ client vs server side
+cs
+ss
+javasript Enginees:
+browser-48- google
+edge
+firefox
+
+
+
+js
+parser
+ast
+interpretere
+macine code
+cpu
+
+
+ECMA-setting standard
+java script should follow js versions:
+
+important features:
+syncronous
+
+limitations:
+cannot directly os files
+read local files freely
+acess heardware directly
+replace system programming
+
+js  environment
+js engine
+variables,datatypes,operataors,functions
+browser:
+borwser object oriented
+document object orirntrd
+timer APIS
+fetch APIS
+local stroge
+
+node.js
+
+file system
+http server
+path
+os
+stream
+process
+
+*/
+
+//js syntax:
+//rules that defines js must follow
+//console.log("hello")
+// node.js as runnig tool
+//
+// rules:
+// case-sensitive ex:john,
+// 
+//key words:
+//let:Can be updated but cannot be redeclared in the same scope
+//
+//js stmt and expressions
+//a stmt tells js to perform an action
+let age=25;
+console.log(age);
+//an expression returns the value always
+
+//js comments:
+//comments makes as code more readable & easier to understand
+// two types of comments-single line & Multilines
+// single line-
+//multiple line-/*  */
+
+// why do we use comments:
+//explain logic
+// improve readability
+//
+//keywords or Reserved keywords:
+//spcl words with 
+//
+//identifiers:
+//
+// names are the identfiers-variables,constants,functions,classes etc
+//
+let age=25;
+const PI=3.14;
+
+//rules and convention
+//
+// rules:
+// allowed characters
+
+//naming convention
+//variables and functions:
+//camelCase:
+let My='amore'
+function calculateIntreset() {
+    console.log('object');
+}
+//pascalCase
+//class and consturctor functions
+//
+//boolean convections 
+//haspermission=false;
+//let isAdmin=true;
+
+//memory Allocation:
+//STACK MEMORY:PRIMETIVE TYPES
+//HEAP MEMORY:OBJECT TYPES
+
+//variables:
+//A variable is a named container used to store data in a program. 
+//The value stored in a variable can be changed during program execution.
+//types
+//var=global var
+//
+// let=local var
+// let=Can be updated but cannot be redeclared in the same scope.
+//
+// const=local variable
+// const=Cannot be updated or redeclared.
+
+//strict mode:
+'use strict';
+
+
+//typescript:

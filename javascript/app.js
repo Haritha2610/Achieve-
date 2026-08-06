377f8b3944a8c225e54b33ac4b393a356956f2eb
Hari G <gudhitiharitha2610@gmail.com>
2026-08-06 11:57:07 +0530

    villa project

--- javascript/app.js
+++ javascript/app.js
@@ -1,171 +1,80 @@
+// let total =0;
 
-// 1. Positive, Negative, or Zero 
-let num = -15;
+// for(let i=0;i<=50;i++){
+//     // total +=1;
+// console.log(i);
+// }
+// // console.log(total);
+// let n=12;
+// for(let i = 1;i<=10;i++){
+//     console.log(`${n} x ${1} = ${n *i}`);
+// }
+// let i= 1;
+// while(i<=10){
+//     console.log(i);
+//     i+=4;
+// }
+// let i=1;
+// do{
+//     console.log(i);
+//     i++;
 
-if (num > 0) {
-    console.log("Positive Number");
-} else if (num < 0) {
-    console.log("Negative Number");
-} else {
-    console.log("Zero");
-}
-console.log("--------------------------------------------");
-// 2 .Largest of Two Numbers 
+// }while(i>=10)
+// let prompt = require("prompt-sync")();
+// let password=" ";
+// do{
+//     password=prompt("enter the password");
 
-let a = 12, b = 25;
-console.log("Largest Number:", a > b ? a : b);
-console.log("--------------------------------------------");
-// 3. Voting Eligibility 
-let age = 19;
-if (age >= 18) {
-    console.log("Eligible to Vote");
-} else {
-    console.log("Not Eligible to Vote");
-}
-console.log("--------------------------------------------");
-//4. Leap Year Checker 
-let year = 2024;
-if ((year % 400 === 0) || (year % 4 === 0 && year % 100 !== 0)) {
-    console.log("Leap Year");
-} else {
-    console.log("Not a Leap Year");
-}
-console.log("--------------------------------------------");
-//5. Grade Calculator 
-let marks = 86;
-if (marks >= 90) console.log("Grade: A");
-else if (marks >= 80) console.log("Grade: B");
-else if (marks >= 70) console.log("Grade: C");
-else if (marks >= 60) console.log("Grade: D");
-else console.log("Fail");
-console.log("--------------------------------------------");
+// }while(password != "1234");
+// console.log(typeof -Infinity);
+// 		console.log(typeof -100);
+// 		console.log(typeof 100);
+// 		console.log(100e5);
 
-//6. Largest of Three Numbers
-let x = 45, y = 78, z = 31;
-let largest = Math.max(x, y, z);
-console.log("Largest Number:", largest);
-console.log("--------------------------------------------");
 
-// 7. Simple Calculator
-let num1 = 15, operator = "*", num2 = 4;
-let result;
-switch (operator) {
-    case "+": result = num1 + num2; break;
-    case "-": result = num1 - num2; break;
-    case "*": result = num1 * num2; break;
-    case "/": result = num1 / num2; break;
-    default: console.log("Invalid Operator");
-}
-console.log("Result:", result);
-console.log("--------------------------------------------");
+		//  console.log(Math.sqrt(-1));
+		//  console.log(parseInt("ABC"));
+		//  console.log(NaN==NaN);
+// 		//   console.log(isNaN(NaN));
+// let num=123.898;
+// 		console.log(num.toPrecision(2));
+// let price=89.89898;
+// 	console.log(price.toFixed(6));
+// let num=123;
+// 		console.log(typeof num.toString());
+// 		console.log(typeof num.toString(12));
+// let balance=225.678;
+// 			console.log(parseInt(balance));
+// 			let balance1=225;
+// 			console.log(parseFloat(balance1))
 
-//8. Character Type Checker
-let ch = 'A';
-if (/[A-Z]/.test(ch)) 
-    console.log("Uppercase Letter");
-else if (/[a-z]/.test(ch)) 
-    onsole.log("Lowercase Letter");
-else if (/[0-9]/.test(ch)) 
-    console.log("Digit");
-else 
-    console.log("Special Character");
-console.log("--------------------------------------------");
+// 			a=Number.MAX_SAFE_INTEGER
+// 			b=Number.MIN_SAFE_INTEGER
+// 			console.log(a-1);
+// 			console.log(b+1);
 
-//9. Divisibility Checker
-let n = 30;
+// let isStudent = true
+// 		let isAdmin = true
+// 		true != true
+// 		false != false
+// 		false != "false"
+// 		true !='true'
+// let student=null;
+// 		// student={
+// 		// 	name="Amar"
+// 		// }
+// 		// student=null;
+// 		console.log(student);
+// 		console.log(typeof student);
 
-if (n % 3 === 0 && n % 5 === 0)
-    console.log("Divisible by both 3 and 5");
-else if (n % 3 === 0)
-    console.log("Divisible by 3");
-else if (n % 5 === 0)
-    console.log("Divisible by 5");
-else
-    console.log("Not divisible by 3 or 5");
-console.log("--------------------------------------------");
+// 		console.log(undefined == null);
+// 		console.log(undefined === null);
 
-//10. Electricity Bill Calculator
+let id1 = Symbol()
+		let id2 = Symbol()
+		console.log(id1 == id2);
 
-let units = 350;
-let billAmount;
 
-if (units <= 100) {
-    billAmount = units * 1.2;
-} else if (units <= 200) {
-    billAmount = 100 * 1.2 + (units - 100) * 2;
-} else {
-    billAmount = 100 * 1.2 + 100 * 2 + (units - 200) * 3;
-}
-
-console.log("Electricity Bill:", billAmount);
-console.log("--------------------------------------------");
-
-// 11. BMI Category 
-let weight = 70; // in kg
-let height = 1.75; // in meters
-let bmi = weight / (height * height);   
-if (bmi < 18.5) {
-    console.log("Underweight");
-}
-else if (bmi < 24.9) {
-    console.log("Normal");
-}   
-else if (bmi < 29.9) {
-    console.log("Overweight");
-}       
-else {
-    console.log("Obese");
-}   
-console.log("--------------------------------------------");
-
-// 12. Triangle Validator 
-let a1 = 60, b1 = 70, c1 = 50;
-if (a1 + b1 + c1 === 180) {
-    console.log("Valid Triangle");
-}
-console.log("--------------------------------------------");
-//13. Discount Calculator
-let amount = 6000;
-let discount=0;
-if (amount >= 10000) 
-    discount = 0.30;
-else if (amount >= 5000) 
-    discount = 0.20;
-else if (amount >= 1000) 
-    discount = 0.10;
-let discountAmount = amount * discount;
-let finalAmount = amount - discountAmount;
-
-console.log("Discount:", discountAmount);
-console.log("Final Amount:", finalAmount);
-console.log("--------------------------------------------");
-//14. Login Verification
-let username = "admin";
-let password = "12345";
-if (username === "admin" && password === "12345") {
-    console.log("Login Successful");
-}
-else if (username !== "admin") {
-    console.log("Invalid Username");
-}   
-else if (password !== "12345") {
-    console.log("Invalid Password");
-}   
-else {
-    console.log("Invalid Credentials");
-}   
-console.log("--------------------------------------------");
-// 15. ATM Withdrawal
-let balance = 5000;
-let withdraw = 1200;
-if (withdraw % 100 !== 0) {
-    console.log("Amount must be multiple of 100");
-} else if (withdraw > balance) {
-    console.log("Insufficient Balance");
-} else if (balance - withdraw < 500) {
-    console.log("Minimum balance of ₹500 required");
-} else {
-    balance -= withdraw;
-    console.log("Withdrawal Successful. Remaining Balance:", balance);
-}
-console.log("--------------------------------------------");
+		let id3 = Symbol("hello")
+		let id4 = Symbol("hello")
+		console.log(id3 === id4);
--- javascript/notes.js
+++ javascript/notes.js
@@ -91,8 +91,8 @@ process
 //
 //js stmt and expressions
 //a stmt tells js to perform an action
-let age=25;
-console.log(age);
+// let age=25;
+// console.log(age);
 //an expression returns the value always
 
 //js comments:
@@ -112,8 +112,8 @@ console.log(age);
 //
 // names are the identfiers-variables,constants,functions,classes etc
 //
-let age=25;
-const PI=3.14;
+// let age=25;
+// const PI=3.14;
 
 //rules and convention
 //
@@ -123,10 +123,10 @@ const PI=3.14;
 //naming convention
 //variables and functions:
 //camelCase:
-let My='amore'
-function calculateIntreset() {
-    console.log('object');
-}
+// let My='amore'
+// function calculateIntreset() {
+//     console.log('object');
+// }
 //pascalCase
 //class and consturctor functions
 //
@@ -151,7 +151,142 @@ function calculateIntreset() {
 // const=Cannot be updated or redeclared.
 
 //strict mode:
-'use strict';
+// 'use strict';
 
 
 //typescript:
+// let name="   hari    ";
+// console.log(name.trim().toUpperCase().padEnd(6,"4"));
+//===============
+//javascript Arrays: is a collection of ordered elements stored in a single variable.
+//  Each element in an array has a numeric index, starting from 0. 
+// Arrays can hold elements of different data types, including numbers, strings, objects, and even other arrays. 
+//arrays are mutable
+//arrays are objects
+//arrays are dynamic
+//arrays can hold different data types,multiple values in a single variable
+
+
+// let students = ["hari", "ram", "shyam", "gita"];
+// console.log(students[0]);
+
+//Indexed-started from 0
+//slicing allowed
+//Array literal
+// array constructor
+// let students = new Array["hari", "ram", "shyam", "gita"];
+// let rollNumbers = new Array[3, 2, 3, 4, 5];
+// console.log(students[0]);
+// console.log(rollNumbers[0]);
+// let cart=[];
+// console.log(cart);
+// //let arr = new Array("Apple", "Banana", "Mango");
+// console.log(arr);
+// let arr = new Array(3);
+// let srr=new arrray("apple,banana,cherry");
+// arr[1] = "orange";
+// console.log(arr); 
+
+// let arr = new Array("Apple","banana","cherry");
+// arr[2]=230;
+// console.log(arr);
+
+//add,remove,search,update ,sort,Reverse, filter, transfer
+// array methods:
+
+//push();-add
+//pop();-delete
+//unshift();
+//shift();
+//indexOf(cart);
+//lastIndexOf()
+//Includes
+//JOIN()
+//slice()
+//splice(how many elements,what to be added) remove and add items at the same time
+//concate()
+//reverse()
+//sort()
+//forEach()
+//Map()
+//filter()
+//find()
+//findIndex()
+//every()
+//reduce()
+//flat()
+//flatMap()
+//fill()
+
+
+// let arr = ["Apple","Banana","cherry"];
+// arr.push("orange");
+// console. log(arr)
+
+// let cart = ["Mobile","Charger","Laptop","charger","Mobile"]
+// console.log(cart.sort());
+
+// cart.splice(2,2,1, "MPL");
+// console.log(cart);
+
+// let permissions=[
+// "READ",
+// "WRITE",
+// "DELETE"
+// ];
+// console.log(typeof permissions);
+// let re = permissions.join(" ");
+// console.log(typeof re);
+
+// let cart =[1,2,3,4,5,6,7,8,9]
+// console.log(cart.sort())
+// console.log(cart.sort((a,b) => (b-a)));
+// let b =[8,4,6,5,3,8,3,1]
+// console.log(a.reverse().slice(0,5));
+// console.log(b.reverse());
+
+
+// let students=[
+//     "hari", "ram", "shyam", "gita"
+
+// ];
+// students.forEach(student => {console.log(students);})
+// let prices =[
+//     100,200,400,];
+
+//     // let gst = prices.map(price => price * 1.18);
+//     let gst = prices.filter(price => price <300);;
+//     console.log(gst);
+// let nums=[
+//     18,
+//     22,
+//     25,
+//     9,
+//     89
+// ];
+// // console.log(ages.every(age => age >= 18));
+// let res = nums.reduce((Sum,num) => sum * num,1);
+// console.log(res);
+//============================
+// javascript object:
+// It is collection of related data stored in the form of keys and values
+//key values must be immutable type
+//values can be any valid js datatype 
+//
+// mutable
+//heterogenous datatypes
+// without Object
+
+// let name="Hari";
+// let age = 23
+// let city = "MPL";
+
+//with array;
+let person={
+    name="Hari",    age = 23,
+    city = "MPL",
+    rollno =195,
+    course="BCA",
+    category = "stats"
+};
+
